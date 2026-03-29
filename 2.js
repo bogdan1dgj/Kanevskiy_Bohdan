547ad94287b13d91a82618a3f6ec64efbd7a0166
bogdan <bogdan.kanevskiy23@gmail.com>
2026-03-29 21:23:55 +0300

    практична 4

---
+++ 2.js
@@ -0,0 +1,32 @@
+const input = document.getElementById("textInput");
+const addButton = document.getElementById("btnAdd");
+const sortButton = document.getElementById("btnSort");
+const list = document.getElementById("itemsList");
+
+addButton.addEventListener("click", () => {
+    const value = input.value.trim();
+
+    if (value === "") return;
+
+    const item = document.createElement("li");
+    item.textContent = value;
+
+    // Видалення при кліку
+    item.addEventListener("click", () => {
+        item.remove();
+    });
+
+    list.appendChild(item);
+    input.value = "";
+});
+
+sortButton.addEventListener("click", () => {
+    const elements = Array.from(list.children);
+
+    elements.sort((a, b) =>
+        a.textContent.localeCompare(b.textContent, 'uk')
+    );
+
+    list.innerHTML = "";
+    elements.forEach(el => list.appendChild(el));
+});
