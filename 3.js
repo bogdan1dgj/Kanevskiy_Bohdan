f75dfa9acc4dbe6e61d87d53067615aab5c39491
bogdan <150701351+bogdan1dgj@users.noreply.github.com>
2026-04-30 14:48:45 +0300

    Update 3.js

--- 3.js
+++ 3.js
@@ -9,12 +9,54 @@ const editPhone = document.getElementById("editPhone");
 const KEY = "user";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const data = JSON.parse(localStorage.getItem(KEY));
-  if (!data) return;
+    const data = JSON.parse(localStorage.getItem(KEY));
+    if (!data) return;
 
-  name.value = data.name;
-  email.value = data.email;
-  phone.value = data.phone;
-  password.value = data.password;
+    name.value = data.name;
+    email.value = data.email;
+    phone.value = data.phone;
+    password.value = data.password;
 });
 
+form.addEventListener("input", () => {
+    const user = {
+        name: name.value,
+        email: email.value,
+        phone: phone.value,
+        password: password.value
+    };
+
+    localStorage.setItem(KEY, JSON.stringify(user));
+});
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    alert("Збережено!");
+});
+
+loginBtn.onclick = () => {
+    const data = JSON.parse(localStorage.getItem(KEY));
+    if (!data) return alert("Немає користувача");
+
+    if (email.value === data.email && password.value === data.password) {
+        profile.classList.remove("hidden");
+        editName.value = data.name;
+        editPhone.value = data.phone;
+    } else {
+        alert("Помилка входу");
+    }
+};
+
+update.onclick = () => {
+    const data = JSON.parse(localStorage.getItem(KEY));
+    data.name = editName.value;
+    data.phone = editPhone.value;
+
+    localStorage.setItem(KEY, JSON.stringify(data));
+    alert("Оновлено");
+};
+
+clearBtn.onclick = () => {
+    localStorage.removeItem(KEY);
+    location.reload();
+};
